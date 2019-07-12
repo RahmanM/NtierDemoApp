d3bde1e113d81b7c4ab2f414877b0cf3bc54d05c
Rahman Mahmoodi <rahman.mahmoodi@gmail.com>
2019-07-12 18:55:45 +1000

    initial

---
+++ SampleNTier.Services/Database/CustomersContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SampleNTier.Services.Database
+{
+    public class CustomersContext : DbContext
+    {
+        public CustomersContext() : base("CustomersConnectionString")
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+    }
+}
